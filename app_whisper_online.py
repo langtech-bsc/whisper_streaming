1703ba59762d0156b126e1cfa0374ba41f04dceb
Martí Umbert <marti.umbert@bsc.es>
2025-03-20 14:20:38 +0100

    app_whisper_online.py: transcribe function used to prepare variables and warm up asr model

--- app_whisper_online.py
+++ app_whisper_online.py
@@ -1,22 +1,50 @@
-from whisper_online import *
+# from whisper_online import *
+import whisper_online as wo
 import librosa
 import datetime
 import spaces
 import torch
+import sys
+
+class ARGS():
+    min_chunk_size = 1.0
+    model = "large-v2"
+    model_cache_dir = None
+    model_dir = None
+    lan = "es"
+    task = "transcribe"
+    backend = "faster-whisper"
+    vac = False
+    vac_chunk_size = 0.04
+    vad = False
+    buffer_trimming = "segment"
+    buffer_trimming_sec = 15
+    log_level = "DEBUG"
+    start_at = 0.0
+    offline = False
+    comp_unaware = False    
+    
+args = ARGS()
+
+logfile, audio_path, duration, online, min_chunk, asr, out_lines = wo.prepare(args)
+start, beg = wo.asr_warmup(asr)
+
+
+#################################################################################################
 
 # src_lan = "en"  # source language
 # tgt_lan = "en"  # target language  -- same as source for ASR, "en" if translate task is used
 
-asr = FasterWhisperASR("ca", "large-v2")  # loads and wraps Whisper model
-# set options:
-# asr.set_translate_task()  # it will translate from lan into English
-# asr.use_vad()  # set using VAD
-logger.info("(logger) ASR model loaded")
-print("(print) ASR model loaded")
+# asr = wo.FasterWhisperASR("ca", "large-v2")  # loads and wraps Whisper model
+# # set options:
+# # asr.set_translate_task()  # it will translate from lan into English
+# # asr.use_vad()  # set using VAD
+# logger.info("(logger) ASR model loaded")
+# print("(print) ASR model loaded")
 
-online = OnlineASRProcessor(asr)  # create processing object with default buffer trimming option
-logger.info("(logger) OnlineASRProcessor loaded")
-print("(print) OnlineASRProcessor loaded")
+# online = wo.OnlineASRProcessor(asr)  # create processing object with default buffer trimming option
+# logger.info("(logger) OnlineASRProcessor loaded")
+# print("(print) OnlineASRProcessor loaded")
 
 # while audio_has_not_ended:   # processing loop:
 # 	a = # receive new audio chunk (and e.g. wait for min_chunk_size seconds first, ...)
@@ -34,39 +62,53 @@ print("(print) OnlineASRProcessor loaded")
 
 import gradio as gr
 
+# @spaces.GPU
+# def transcribe(stream, new_chunk):
+
+#     print(f"transcribe cuda available: {torch.cuda.is_available()}")
+
+#     sr, y = new_chunk
+#     print(f"transcribe() called: {sr}")
+
+
+#     # Convert to mono if stereo
+#     if y.ndim > 1:
+#         y = y.mean(axis=1)
+
+#     y = y.astype(np.float32)
+#     # y /= np.max(np.abs(y))
+
+#     print(type(y))
+#     if sr != 16000:
+#             print(f"resampling() called")
+#             y = librosa.resample(y=y, orig_sr=sr, target_sr=16000)
+
+#     #print(f"[app ASR RT] info: sr={sr}\tlen(y)={len(y)}")
+#     # 
+
+#     if stream is not None:
+#         stream = np.concatenate([stream, y])
+#     else:
+#         stream = y
+
+#     # online.insert_audio_chunk(stream)
+#     # o = online.process_iter()
+
+#     return stream, datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") # o
+
+
+
 @spaces.GPU
-def transcribe(stream, new_chunk):
+def transcribe(mic=None, file=None):
 
-    print(f"transcribe cuda available: {torch.cuda.is_available()}")
-
-    sr, y = new_chunk
-    print(f"transcribe() called: {sr}")
-
-
-    # Convert to mono if stereo
-    if y.ndim > 1:
-        y = y.mean(axis=1)
-
-    y = y.astype(np.float32)
-    # y /= np.max(np.abs(y))
-
-    print(type(y))
-    if sr != 16000:
-            print(f"resampling() called")
-            y = librosa.resample(y=y, orig_sr=sr, target_sr=16000)
-
-    #print(f"[app ASR RT] info: sr={sr}\tlen(y)={len(y)}")
-    # 
-
-    if stream is not None:
-        stream = np.concatenate([stream, y])
+    if mic is not None:
+        audio = mic
+    elif file is not None:
+        audio = file
     else:
-        stream = y
+        return "You must either provide a mic recording or a file"
+    
 
-    # online.insert_audio_chunk(stream)
-    # o = online.process_iter()
-
-    return stream, datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") # o
 
 def create_app():
 
@@ -77,10 +119,19 @@ def create_app():
             Speak and transcribe your speech
             """
         )
+        # gr.Interface(
+        #     transcribe,
+        #     ["state", gr.Audio(sources=["microphone"], streaming=True)],
+        #     ["state", "text"],
+        #     live=True,
+        # )
         gr.Interface(
-            transcribe,
-            ["state", gr.Audio(sources=["microphone"], streaming=True)],
-            ["state", "text"],
+            fn=transcribe,
+            inputs=[
+                gr.Audio(sources="microphone", type="filepath"),
+                gr.Audio(sources="upload", type="filepath"),
+            ],
+            outputs="text",
             live=True,
         )
 

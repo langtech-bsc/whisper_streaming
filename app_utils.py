0252bfbde47551f94e1cede898c52ae274ff4a5c
Martí Umbert <marti.umbert@bsc.es>
2025-03-24 11:01:30 +0100

    create app_utils to load ARGS in all files, reorganize visually the app

---
+++ app_utils.py
@@ -0,0 +1,19 @@
+
+class ARGS():
+    audio_path = None
+    min_chunk_size = 1.0
+    model = "large-v2"
+    model_cache_dir = None
+    model_dir = None
+    lan = "es"
+    task = "transcribe"
+    backend = "faster-whisper"
+    vac = False
+    vac_chunk_size = 0.04
+    vad = False
+    buffer_trimming = "segment"
+    buffer_trimming_sec = 15
+    log_level = "DEBUG"
+    start_at = 0.0
+    offline = False
+    comp_unaware = False    
